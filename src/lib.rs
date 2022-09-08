93acf441e150230c122b71895ae2a55c91d16bfc
Aaron Tsui <aaron.tsui@outlook.com>
2022-09-08 10:13:29 +0800

    return topic type

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,14 @@ pub use common::is_valid_mqtt_topic;
 pub use common::is_valid_prefix;
 pub use common::is_valid_shadow_name;
 pub use common::is_valid_thing_name;
+
+pub enum TopicType {
+    Shadow = 0,
+    Jobs,
+    Defender,
+    Tunneling,
+}
+
+pub fn match_topic<'a>(topic: &'a str) -> Result<TopicType, Error> {
+    Ok(TopicType::Shadow)
+}
